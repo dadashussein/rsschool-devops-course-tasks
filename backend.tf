0b7caeb76f8eeb3401d5a1e2e1df2cf7cacca701
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-09-26 10:12:21 +0400

    done terraform

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "rsschool-devops-course-tasks-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- main.tf
+++ main.tf
@@ -3,25 +3,9 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "rsschool-devops-course-tasks-state"
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "example" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
+  bucket = "dadodevops"
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Production"
   }
 }
