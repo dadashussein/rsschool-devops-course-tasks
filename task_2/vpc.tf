f67653887a8596706f591edeaaa7b7c360cb2cab
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-10-12 17:32:54 +0400

    update subnet name

--- task_2/vpc.tf
+++ task_2/vpc.tf
@@ -12,7 +12,10 @@ resource "aws_subnet" "public_subnet_1" {
   cidr_block              = var.public_subnet_1_cidr
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
-  tags                    = var.tags
+  
+  tags = merge(var.tags, {
+    Name = "Public Subnet 1"
+  })
 }
 
 resource "aws_subnet" "public_subnet_2" {
@@ -20,21 +23,30 @@ resource "aws_subnet" "public_subnet_2" {
   cidr_block              = var.public_subnet_2_cidr
   availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = true
-  tags                    = var.tags
+  
+  tags = merge(var.tags, {
+    Name = "Public Subnet 2"
+  })
 }
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = var.private_subnet_1_cidr
   availability_zone = "eu-central-1a"
-  tags              = var.tags
+  
+  tags = merge(var.tags, {
+    Name = "Private Subnet 1"
+  })
 }
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = var.private_subnet_2_cidr
   availability_zone = "eu-central-1b"
-  tags              = var.tags
+  
+  tags = merge(var.tags, {
+    Name = "Private Subnet 2"
+  })
 }
 
 
